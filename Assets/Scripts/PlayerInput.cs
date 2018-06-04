66d728b944564663bf72e4dfa8765caec0e32289
Talha Qamar <boooooooofys@gmail.com>
2018-06-04 20:42:53 +0500

    Movement Of Player(Not Camera)

---
+++ Assets/Scripts/PlayerInput.cs
@@ -0,0 +1,31 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+[RequireComponent(typeof(PlayerMotor))]
+public class PlayerInput : MonoBehaviour {
+	float hori; //Horizontal Axis
+	float vert; //Vertical Axis
+	[SerializeField]float ispeed; //Speed given from editor
+
+	private PlayerMotor motor;
+
+	void Start(){
+		motor = GetComponent<PlayerMotor> ();
+	}
+
+	// Update is called once per frame
+	void Update () {
+		//Assigns the Axis to the previously created variables
+		hori = Input.GetAxisRaw ("Horizontal");
+		vert = Input.GetAxisRaw ("Vertical");
+		//Creates velocity vectors
+		Vector3 _movHori = transform.right * hori;
+		Vector3 _movVert = transform.forward * vert;
+
+		Vector3 _velocity = (_movHori + _movVert).normalized * ispeed;
+
+		motor.Move(_velocity);
+  }
+
+
+}
---
+++ Assets/Scripts/PlayerMotor.cs
@@ -0,0 +1,40 @@
+﻿using UnityEngine;
+
+[RequireComponent(typeof(Rigidbody))]
+public class PlayerMotor : MonoBehaviour {
+
+	private Vector3 velocity = Vector3.zero;
+
+
+
+
+	private Rigidbody rb;
+
+	void Start ()
+	{
+		rb = GetComponent<Rigidbody>();
+	}
+
+	// Gets a movement vector
+	public void Move (Vector3 _velocity)
+	{
+		velocity = _velocity;
+	}
+
+
+
+	// Run every physics iteration
+	void FixedUpdate ()
+	{
+		PerformMovement();
+
+	}
+
+	//Perform movement based on velocity variable
+	void PerformMovement ()
+	{
+		if (velocity != Vector3.zero) {
+			rb.MovePosition (rb.position + velocity * Time.fixedDeltaTime);
+		}
+	}		
+}
